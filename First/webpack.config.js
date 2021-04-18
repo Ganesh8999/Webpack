1495edfb44ec52c2c9f0cf3ddde9d722252f7b26
Ganesh8999 <Ganesh8999@github.com>
2021-04-18 17:58:19 +0530

    added svg-inline,css,style,babel loaders

---
+++ First/webpack.config.js
@@ -0,0 +1,19 @@
+const { Module } = require("webpack");
+
+module.exports = {
+  entry: "./app/index.js",
+
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        // loader: "svg-inline-loader",
+        use: "svg-inline-loader", // recommend to use"
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
